12372b20f60c4a16f09545a8af7f9ceaa88f5d9c
kje <kje0740@gmail.com>
2024-10-29 14:01:57 +0900

    git연동

---
+++ AniLogAPI/src/main/java/com/recojin/anilogapi/AniLogApiApplication.java
@@ -0,0 +1,13 @@
+package com.recojin.anilogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniLogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniLogApiApplication.class, args);
+	}
+
+}
---
+++ AniLogAPI/src/test/java/com/recojin/anilogapi/AniLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recojin.anilogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniLogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
